ef62201fc75fddab2c749e68f3078c1acf841c58
Mohammad Shalabi <shalabicloud@gmail.com>
2020-11-28 13:22:29 +0100

    remove imports

--- mdc/top-bar/app.js
+++ mdc/top-bar/app.js
@@ -1,5 +1,6 @@
 import {MDCTopAppBar} from '@material/top-app-bar';
 import {MDCDrawer} from "@material/drawer";
+import {MDCMenu} from '@material/menu';
 
 const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
 const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
@@ -7,5 +8,10 @@ const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
 topAppBar.listen('MDCTopAppBar:nav', () => {
     console.log('menu clicked state of drawer ' + drawer.open);
     drawer.open = !drawer.open;
+});
+
+document.querySelector('#menu-button').addEventListener("click", () => {
+    const menu = new MDCMenu(document.querySelector('.mdc-menu'));
+    menu.open = !menu.open;
 });
 
